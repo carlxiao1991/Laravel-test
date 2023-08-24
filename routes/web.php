baacd4f814ffd54c91997a9799986093debbd42a
xiaowentao <xiaowentao@eccang.com>
2023-08-24 22:23:35 +0800

    test request

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Http\Helpers\ApiResponse;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Validation\ValidationException;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +18,31 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/api/request', function (Request $request) {
+    $data = $request->all();
+    return ApiResponse::format($data);
+});
+
+    Route::get('/api/validation-error', function () {
+    throw ValidationException::withMessages([
+        'field_name' => ['Validation error message'],
+    ]);
+});
+
+Route::get('/api/unexpected-error', function () {
+    throw new Exception('Unexpected error occurred.');
+});
+
+Route::get('/api/logical-test', function (Request $request) {
+    $s = $request->query('s');
+
+    if ($s === 'example') {
+        $data = 'Logical test passed.';
+    } else {
+        $data = 'Logical test failed.';
+    }
+
+    return ApiResponse::format($data);
 });
